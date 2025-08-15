683bc7319df4a879d94dcec7600f7441c0c668b7
eunchan96 <taegwon02@gmail.com>
2025-08-15 16:36:50 +0900

    openapi-fetch

--- frontend/src/lib/backend/client.ts
+++ frontend/src/lib/backend/client.ts
@@ -1,4 +1,15 @@
+import createClient from "openapi-fetch";
+
+import type { paths } from "@/lib/backend/apiV1/schema";
+
 const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const client = createClient<paths>({
+  baseUrl: NEXT_PUBLIC_API_BASE_URL,
+  credentials: "include",
+});
+
+export default client;
 
 export const apiFetch = (url: string, options?: RequestInit) => {
   options = options || {};
