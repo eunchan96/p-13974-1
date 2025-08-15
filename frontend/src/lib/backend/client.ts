2e082173a7ef1ed11614f6318505ea5425263576
eunchan96 <taegwon02@gmail.com>
2025-08-15 17:28:03 +0900

    apiFetch 삭제

--- frontend/src/lib/backend/client.ts
+++ frontend/src/lib/backend/client.ts
@@ -9,29 +9,3 @@ const client = createClient<paths>({
 });
 
 export default client;
-
-export const apiFetch = (url: string, options?: RequestInit) => {
-  options = options || {};
-
-  options.credentials = "include";
-
-  if (options?.body) {
-    const headers = new Headers(options?.headers || {});
-
-    if (!headers.has("Content-Type")) {
-      headers.set("Content-Type", "application/json; charset=utf-8");
-    }
-
-    options.headers = headers;
-  }
-
-  return fetch(`${NEXT_PUBLIC_API_BASE_URL}${url}`, options).then((res) => {
-    if (!res.ok) {
-      return res.json().then((errorData) => {
-        throw errorData;
-      });
-    }
-
-    return res.json();
-  });
-};
