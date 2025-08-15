b8448f05271cf2d042a6c967f8fabcc5a4b6b916
eunchan96 <taegwon02@gmail.com>
2025-08-15 17:36:00 +0900

    posts/[id]/_hooks

---
+++ frontend/src/app/posts/[id]/_hooks/usePost.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import client from "@/lib/backend/client";
+import type { components } from "@/lib/backend/apiV1/schema";
+
+type PostWithContentDto = components["schemas"]["PostWithContentDto"];
+
+export default function usePost(id: number) {
+  const [post, setPost] = useState<PostWithContentDto | null>(null);
+
+  useEffect(() => {
+    client
+      .GET("/api/v1/posts/{id}", {
+        params: {
+          path: {
+            id,
+          },
+        },
+      })
+      .then((res) => {
+        if (res.error) {
+          alert(`${res.error.resultCode} : ${res.error.msg}`);
+          return;
+        }
+        setPost(res.data);
+      });
+  }, [id]);
+
+  const deletePost = (onSuccess: () => void) => {
+    client
+      .DELETE("/api/v1/posts/{id}", {
+        params: {
+          path: {
+            id,
+          },
+        },
+      })
+      .then((res) => {
+        if (res.error) {
+          alert(`${res.error.resultCode} : ${res.error.msg}`);
+          return;
+        }
+        onSuccess();
+      });
+  };
+
+  return { id, post, deletePost };
+}
